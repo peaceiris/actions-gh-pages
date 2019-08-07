77beeabda14ee15a02520af84590420476f55e86
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-08-07 22:11:32 +0900

    enhance: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
 
-# setup ssh
+# check values
 if [ -z "${GITHUB_TOKEN}" ]; then
     echo "error: not found GITHUB_TOKEN"
     exit 1
 fi
 
-# push to gh-pages branch
+if [ -z "${PUBLISH_BRANCH}" ]; then
+    echo "error: not found PUBLISH_BRANCH"
+    exit 1
+fi
+
 if [ -z "${PUBLISH_DIR}" ]; then
     echo "error: not found PUBLISH_DIR"
     exit 1
 fi
 cd "${PUBLISH_DIR}" || exit 1
-if [ -z "${PUBLISH_BRANCH}" ]; then
-    echo "error: not found PUBLISH_BRANCH"
-    exit 1
-fi
+
+# initialize git
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_branch="${PUBLISH_BRANCH}"
 git init
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git remote add origin "${remote_repo}"
+
+# push to publishing branch
 git checkout "${remote_branch}" || git checkout --orphan "${remote_branch}"
 git add --all
 timestamp=$(date -u)
