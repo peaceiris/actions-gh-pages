766f94d9180cb0325d423c05eddb129e1b2d7a35
Michael Smith <michael.smith@puppet.com>
2019-08-23 10:36:56 -0700

    Fetch remote before testing whether the publish branch exists
    This re-uses the existing branch rather than always overwriting it with
    a new branch with single commit.
    
    Signed-off-by: Michael Smith <michael.smith@puppet.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 # check values
 if [ -z "${GITHUB_TOKEN}" ]; then
     echo "error: not found GITHUB_TOKEN"
@@ -15,19 +17,25 @@ if [ -z "${PUBLISH_DIR}" ]; then
     echo "error: not found PUBLISH_DIR"
     exit 1
 fi
-cd "${PUBLISH_DIR}" || exit 1
 
-# initialize git
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_branch="${PUBLISH_BRANCH}"
-git init
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git remote add origin "${remote_repo}"
+
+local_dir="$(uuidgen)"
+if git clone --single-branch --branch "${remote_branch}" "${remote_repo}" "${local_dir}"; then
+    cd "${local_dir}"
+    git rm -r '*'
+    cp -rf "${PUBLISH_DIR}"/* "${PUBLISH_DIR}"/.* .
+else
+    cd "${PUBLISH_DIR}"
+    git init
+    git remote add origin "${remote_repo}"
+    git checkout --orphan "${remote_branch}"
+fi
 
 # push to publishing branch
-git checkout "${remote_branch}" || git checkout --orphan "${remote_branch}"
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add --all
-timestamp=$(date -u)
-git commit -m "Automated deployment: ${timestamp} ${GITHUB_SHA}"
+git commit -m "Automated deployment: $(date -u) ${GITHUB_SHA}"
 git push origin "${remote_branch}" --force
