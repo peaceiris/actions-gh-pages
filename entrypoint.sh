2b39ba3849506a482c62922290e43169b9020137
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-08-24 16:12:52 +0900

    enhance: change base docker image (ubuntu:18.04), use Bash, fix path
    test: path
    
    test: path
    
    test: path
    
    test: path
    
    test: path
    
    test: path
    
    test: path
    
    test: path
    
    test: path
    
    test: path
    
    test: path
    
    test: path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+set -ex
 
 # check values
 if [ -z "${GITHUB_TOKEN}" ]; then
@@ -21,21 +21,22 @@ fi
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_branch="${PUBLISH_BRANCH}"
 
-local_dir="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)"
-if git clone --single-branch --branch "${remote_branch}" "${remote_repo}" "${local_dir}"; then
+local_dir="${HOME}/$(tr -cd 'a-f0-9' < /dev/urandom | head -c 32)"
+if git clone --depth=1 --single-branch --branch "${remote_branch}" "${remote_repo}" "${local_dir}"; then
     cd "${local_dir}"
     git rm -r '*'
-    cp -rf "${PUBLISH_DIR}"/* "${PUBLISH_DIR}"/.* .
+    cp -rf $(find "${GITHUB_WORKSPACE}/${PUBLISH_DIR}" -maxdepth 1 | tail -n +2) "${local_dir}/"
 else
     cd "${PUBLISH_DIR}"
     git init
-    git remote add origin "${remote_repo}"
     git checkout --orphan "${remote_branch}"
 fi
 
 # push to publishing branch
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git remote rm origin || true
+git remote add origin "${remote_repo}"
 git add --all
 git commit -m "Automated deployment: $(date -u) ${GITHUB_SHA}"
-git push origin "${remote_branch}" --force
+git push origin "${remote_branch}"
