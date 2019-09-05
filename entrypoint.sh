e17751725f155aaf5d8d706fc9b7620240640853
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-09-05 15:55:45 +0900

    revert: ssh deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,25 @@ function print_info() {
 }
 
 # check values
-if [ -z "${GITHUB_TOKEN}" ]; then
-    print_error "not found GITHUB_TOKEN"
+if [ -n "${ACTIONS_DEPLOY_KEY}" ]; then
+
+    print_info "setup with ACTIONS_DEPLOY_KEY"
+
+    mkdir /root/.ssh
+    ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
+    echo "${ACTIONS_DEPLOY_KEY}" > /root/.ssh/id_rsa
+    chmod 400 /root/.ssh/id_rsa
+
+    remote_repo="git@github.com:${GITHUB_REPOSITORY}.git"
+
+elif [ -n "${GITHUB_TOKEN}" ]; then
+
+    print_info "setup with GITHUB_TOKEN"
+
+    remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+else
+    print_error "not found ACTIONS_DEPLOY_KEY or GITHUB_TOKEN"
     exit 1
 fi
 
@@ -27,7 +44,6 @@ if [ -z "${PUBLISH_DIR}" ]; then
     exit 1
 fi
 
-remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_branch="${PUBLISH_BRANCH}"
 
 local_dir="${HOME}/$(tr -cd 'a-f0-9' < /dev/urandom | head -c 32)"
