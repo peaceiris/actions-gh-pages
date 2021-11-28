26852ea92c125eecd304fa261bba1d396b10c881
Kipras Melnikovas <kipras@kipras.org>
2021-11-28 14:35:27 +0200

    fix: use a randomized name for the ssh socket
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- src/set-tokens.ts
+++ src/set-tokens.ts
@@ -59,8 +59,9 @@ Watch https://github.com/peaceiris/actions-gh-pages/issues/87
     await exec.exec('sc', ['config', 'ssh-agent', 'start=auto']);
     await exec.exec('sc', ['start', 'ssh-agent']);
   }
-  await cpexec('ssh-agent', ['-a', '/tmp/ssh-auth.actions-gh-pages.sock']);
-  core.exportVariable('SSH_AUTH_SOCK', '/tmp/ssh-auth.actions-gh-pages.sock');
+  const socketPath = `/tmp/ssh-auth.actions-gh-pages.${Math.random().toString().slice(2)}.sock`;
+  await cpexec('ssh-agent', ['-a', socketPath]);
+  core.exportVariable('SSH_AUTH_SOCK', socketPath);
   await exec.exec('ssh-add', [idRSA]);
 
   return `git@${getServerUrl().host}:${publishRepo}.git`;
